f26e280a7e67707a14c235f3dc6b826cce2be10b
Harmen Mojet <h.mojet@syslogic.nl>
2014-04-14 16:04:56 +0200

    Added getter methods for definition

--- DocumentExportDefinition/DocumentDefinition.php
+++ DocumentExportDefinition/DocumentDefinition.php
@@ -8,10 +8,18 @@ class DocumentDefinition
 	/**
 	 * @Serializer\SerializedName("sections")
 	 * @Serializer\Type("array<DocumentExportDefinition\SectionDefinition>")
-	 * @var array
+	 * @var SectionDefinition[]
 	 */
 	protected $_sections = array();
-	
+
+	const OPTION_ADD_BREAKS_BETWEEN_SECTIONS = "addBreaksBetweenSections";
+	const OPTION_ADD_TITLES = "addTitles";
+
+	protected $_options = [
+			self::OPTION_ADD_TITLES => true,
+			self::OPTION_ADD_BREAKS_BETWEEN_SECTIONS => true
+		];
+
 	public function addSection(SectionDefinition $section)
 	{
 		$this->_sections[] = $section;
@@ -23,5 +31,25 @@ class DocumentDefinition
 	public function getSections()
 	{
 		return $this->_sections;
-	}	
+	}
+
+	public function setOption($option, $value)
+	{
+		if(array_key_exists($option, $this->_options) === false)
+		{
+			throw new \LogicException(sprintf("Invalid option %s specified", $option));
+		}
+
+		$this->_options[$option] = $value;
+	}
+
+	public function getOption($option)
+	{
+		if(array_key_exists($option, $this->_options) === false)
+		{
+			throw new \LogicException(sprintf("Invalid option %s specified", $option));
+		}
+
+		return $this->_options[$option];
+	}
 }
