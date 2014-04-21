05691339b89dabaeccf4366dd1702b51b7d0bc67
HarmenM <h.mojet@syslogic.nl>
2014-04-21 11:01:22 +0200

    Exceptions and options added

--- DocumentExportDefinition/DocumentDefinition.php
+++ DocumentExportDefinition/DocumentDefinition.php
@@ -2,6 +2,7 @@
 namespace DocumentExportDefinition;
 
 use JMS\Serializer\Annotation as Serializer;
+use DocumentExportDefinition\Exception\InvalidArgumentException;
 
 class DocumentDefinition
 {
@@ -18,6 +19,14 @@ class DocumentDefinition
 	const OPTION_FOOTER_TEXT = "footerText";
 	const OPTION_ADD_PAGE_NUMBERING = "addPageNumbering";
 
+	const PROPERTY_TITLE = "title";
+	const PROPERTY_CREATOR = "creator";
+
+	/**
+	 * @Serializer\SerializedName("options")
+	 * @Serializer\Type("array")
+	 * @var array
+	 */
 	protected $_options = [
 			self::OPTION_ADD_TITLES => true,
 			self::OPTION_ADD_BREAKS_BETWEEN_SECTIONS => true,
@@ -26,11 +35,21 @@ class DocumentDefinition
 			self::OPTION_ADD_PAGE_NUMBERING => true
 		];
 
+	/**
+	 * @Serializer\SerializedName("documentProperties")
+	 * @Serializer\Type("array")
+	 * @var array
+	 */
+	protected $_documentProperties = [
+			self::PROPERTY_TITLE => null,
+			self::PROPERTY_CREATOR => null
+		];
+
 	public function addSection(SectionDefinition $section)
 	{
 		$this->_sections[] = $section;
 	}
-	
+
 	/**
 	 * @return SectionDefinition[]
 	 */
@@ -43,7 +62,7 @@ class DocumentDefinition
 	{
 		if(array_key_exists($option, $this->_options) === false)
 		{
-			throw new \LogicException(sprintf("Invalid option %s specified", $option));
+			throw new InvalidArgumentException(sprintf("Invalid option %s specified", $option));
 		}
 
 		$this->_options[$option] = $value;
@@ -53,9 +72,29 @@ class DocumentDefinition
 	{
 		if(array_key_exists($option, $this->_options) === false)
 		{
-			throw new \LogicException(sprintf("Invalid option %s specified", $option));
+			throw new InvalidArgumentException(sprintf("Invalid option %s specified", $option));
 		}
 
 		return $this->_options[$option];
+	}
+
+	public function setDocumentProperty($property, $value)
+	{
+		if(array_key_exists($property, $this->_documentProperties) === false)
+		{
+			throw new InvalidArgumentException(sprintf("Invalid property %s specified", $property));
+		}
+
+		$this->_documentProperties[$property] = $value;
+	}
+
+	public function getDocumentProperty($property)
+	{
+		if(array_key_exists($property, $this->_documentProperties) === false)
+		{
+			throw new InvalidArgumentException(sprintf("Invalid option %s specified", $property));
+		}
+
+		return $this->_documentProperties[$property];
 	}
 }
---
+++ DocumentExportDefinition/Exception/InvalidArgumentException.php
@@ -0,0 +1,7 @@
+<?php
+namespace DocumentExportDefinition\Exception;
+
+class InvalidArgumentException extends \InvalidArgumentException
+{
+	//
+}
