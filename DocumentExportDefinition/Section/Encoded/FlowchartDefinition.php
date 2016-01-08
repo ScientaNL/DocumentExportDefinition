15dab191b3932230be6d4220002b882ae34ac99e
Hikariii <sderooij.git@gmail.com>
2016-01-08 11:46:13 +0100

    Flowchart data added to definition

--- DocumentExportDefinition/Section/Encoded/FlowchartDefinition.php
+++ DocumentExportDefinition/Section/Encoded/FlowchartDefinition.php
@@ -3,8 +3,73 @@ namespace DocumentExportDefinition\Section\Encoded;
 
 class FlowchartDefinition extends AbstractEncodedDataDefinition
 {
+	/**
+	 * @Serializer\SerializedName("mimeType")
+	 * @Serializer\Type("string")
+	 */
+	protected $mimeType;
+
+	/**
+	 * @Serializer\SerializedName("width")
+	 * @Serializer\Type("int")
+	 */
+	protected $width;
+	/**
+	 * @Serializer\SerializedName("height")
+	 * @Serializer\Type("int")
+	 */
+	protected $height;
+
 	public function getType()
 	{
 		return 'FlowchartDefinition';
+	}
+
+	/**
+	 * @param int $mimeType
+	 */
+	public function setMimeType($mimeType)
+	{
+		$this->mimeType = $mimeType;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getMimeType()
+	{
+		return $this->mimeType;
+	}
+
+	/**
+	 * @param int $width
+	 */
+	public function setWidth($width)
+	{
+		$this->width = $width;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getWidth()
+	{
+		return $this->width;
+	}
+
+	/**
+	 * @param int $height
+	 */
+	public function setHeight($height)
+	{
+		$this->height = $height;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getHeight()
+	{
+		return $this->height;
 	}
 }
