eabeced81ee3d444149942b8d2aa2b6ca7c44485
Kars Barendrecht | Syslogic <k.barendrecht@syslogic.nl>
2017-06-20 13:55:17 +0200

    removed def

--- DocumentExportDefinition/Section/AbstractDataDefinition.php
+++ DocumentExportDefinition/Section/AbstractDataDefinition.php
@@ -5,14 +5,14 @@ use JMS\Serializer\Annotation as Serializer;
 
 /**
  * @Serializer\Discriminator(field = "type", map = {
- *    "StringDefinition" :  "DocumentExportDefinition\Section\Data\StringDefinition",
- *    "FileDefinition": "DocumentExportDefinition\Section\Data\FileDefinition",
- *    "TOCDefinition": "DocumentExportDefinition\Section\Data\TOCDefinition",
- *    "DocxDefinition": "DocumentExportDefinition\Section\Encoded\DocxDefinition",
- *    "FlowchartDefinition": "DocumentExportDefinition\Section\Encoded\FlowchartDefinition",
- *    "ImageDefinition": "DocumentExportDefinition\Section\Encoded\ImageDefinition",
- *    "HtmlDefinition": "DocumentExportDefinition\Section\Html\HtmlDefinition",
- *    "HtmlImageDefinition": "DocumentExportDefinition\Section\Html\HtmlImageDefinition"
+ *    "String" :  "DocumentExportDefinition\Section\Data\StringDefinition",
+ *    "File": "DocumentExportDefinition\Section\Data\FileDefinition",
+ *    "TOC": "DocumentExportDefinition\Section\Data\TOCDefinition",
+ *    "Docx": "DocumentExportDefinition\Section\Encoded\DocxDefinition",
+ *    "Flowchart": "DocumentExportDefinition\Section\Encoded\FlowchartDefinition",
+ *    "Image": "DocumentExportDefinition\Section\Encoded\ImageDefinition",
+ *    "Html": "DocumentExportDefinition\Section\Html\HtmlDefinition",
+ *    "HtmlImage": "DocumentExportDefinition\Section\Html\HtmlImageDefinition"
  * })
  */
 abstract class AbstractDataDefinition
@@ -28,6 +28,7 @@ abstract class AbstractDataDefinition
     /**
      * @Serializer\VirtualProperty
      * @Serializer\SerializedName("type")
+     * @Serializer\Type("string")
      * @return string
      */
     abstract public function getType();
