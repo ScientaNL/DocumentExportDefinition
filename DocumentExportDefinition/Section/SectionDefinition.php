babb0a0c43987aa0a055e9e1c564094ab1f92e8b
Kars Barendrecht | Syslogic <k.barendrecht@syslogic.nl>
2017-06-20 16:27:59 +0200

    changed nextsectionorientation

--- DocumentExportDefinition/Section/SectionDefinition.php
+++ DocumentExportDefinition/Section/SectionDefinition.php
@@ -7,29 +7,42 @@ use DocumentExportDefinition\Section\AbstractDataDefinition;
 
 class SectionDefinition
 {
-	/**
-	 * @Serializer\SerializedName("title")
-	 * @Serializer\Type("DocumentExportDefinition\Section\AbstractDataDefinition")
-	 */
-	protected $title;
+    const OPTION_ADD_TITLES = 'addTitles';
+    const OPTION_ADD_PAGE_BREAK = 'addPageBreak';
+    const OPTION_ORIENTATION = 'sectionOrientation';
+    const OPTION_PAGE_NUMBERING = 'pageNumbering';
+    const OPTION_DOWNLOAD_WORD_IMAGES = 'downloadWordImages';
+    /**
+     * @Serializer\SerializedName("title")
+     * @Serializer\Type("DocumentExportDefinition\Section\AbstractDataDefinition")
+     */
+    protected $title;
 
-	/**
-	 * @Serializer\SerializedName("contents")
-	 * @Serializer\Type("DocumentExportDefinition\Section\AbstractDataDefinition")
-	 */
-	protected $contents;
+    /**
+     * @Serializer\SerializedName("contents")
+     * @Serializer\Type("DocumentExportDefinition\Section\AbstractDataDefinition")
+     */
+    protected $contents;
 
-	/**
-	 * @Serializer\SerializedName("variables")
-	 * @Serializer\Type("array<DocumentExportDefinition\Section\AbstractDataDefinition>")
-	 */
-	protected $variables;
+    /**
+     * @Serializer\SerializedName("variables")
+     * @Serializer\Type("array<DocumentExportDefinition\Section\AbstractDataDefinition>")
+     */
+    protected $variables;
 
     /**
      * @Serializer\SerializedName("options")
      * @Serializer\Type("array")
      */
-    protected $options;
+
+    /** @var array */
+    protected $options = [
+        self::OPTION_ADD_TITLES => null,
+        self::OPTION_ADD_PAGE_BREAK => null,
+        self::OPTION_ORIENTATION => null,
+        self::OPTION_PAGE_NUMBERING => null,
+        self::OPTION_DOWNLOAD_WORD_IMAGES => null
+    ];
 
     /**
      * @Serializer\SerializedName("images")
@@ -38,64 +51,64 @@ class SectionDefinition
     protected $images;
 
 
-	/**
-	 * AbstractSectionDefinition constructor.
-	 * @param null $title
-	 * @param null $contents
-	 */
-	public function __construct($title = null, $contents = null)
-	{
-		$this->title = $title;
-		$this->contents = $contents;
-	}
+    /**
+     * AbstractSectionDefinition constructor.
+     * @param null $title
+     * @param null $contents
+     */
+    public function __construct($title = null, $contents = null)
+    {
+        $this->title = $title;
+        $this->contents = $contents;
+    }
 
-	/**
-	 * @param string $title
-	 */
-	public function setTitle($title)
-	{
-		$this->title = $title;
-	}
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getTitle()
-	{
-		return $this->title;
-	}
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
 
-	/**
-	 * @param string $contents
-	 */
-	public function setContents($contents)
-	{
-		$this->contents = $contents;
-	}
+    /**
+     * @param string $contents
+     */
+    public function setContents($contents)
+    {
+        $this->contents = $contents;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getContents()
-	{
-		return $this->contents;
-	}
+    /**
+     * @return string
+     */
+    public function getContents()
+    {
+        return $this->contents;
+    }
 
-	/**
-	 * @param $variables
-	 */
-	public function setVariables($variables)
-	{
-		$this->variables = $variables;
-	}
+    /**
+     * @param $variables
+     */
+    public function setVariables($variables)
+    {
+        $this->variables = $variables;
+    }
 
-	/**
-	 * @return string
-	 */
-	public function getVariables()
-	{
-		return $this->variables;
-	}
+    /**
+     * @return string
+     */
+    public function getVariables()
+    {
+        return $this->variables;
+    }
 
     /**
      * @param $options
