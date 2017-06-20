7186fc9f9dcdf786b43d142d755c8044affa423a
Kars Barendrecht | Syslogic <k.barendrecht@syslogic.nl>
2017-06-20 15:38:22 +0200

    OptionDefinition getter setters added

--- DocumentExportDefinition/Section/Data/OptionsDefinition.php
+++ DocumentExportDefinition/Section/Data/OptionsDefinition.php
@@ -9,12 +9,43 @@ class OptionsDefinition extends AbstractDataDefinition
     /**
      * @Serializer\SerializedName("value")
      * @Serializer\Type("array")
-     * @var string
      */
     protected $value;
 
     public function getType()
     {
         return 'OptionsDefinition';
+    }
+
+    /**
+     * @return string
+     */
+    public function getOption($optionName)
+    {
+        return $this->value[$optionName];
+    }
+
+    /**
+     * @return string
+     */
+    public function setOption($optionName, $optionValue)
+    {
+        $this->value[$optionName] = $optionValue;
+    }
+    
+    /**
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @return array
+     */
+    public function setOptions($options)
+    {
+        $this->value = $options;
     }
 }
