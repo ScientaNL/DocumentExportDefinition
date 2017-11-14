0a9eeef44f24c387d750533ffcc755fe00c71bae
kbarendrecht <k.barendrecht@syslogic.nl>
2017-11-14 14:57:41 +0100

    added badgecategorytrail datatype

---
+++ DocumentExportDefinition/Section/Data/BadgeCategoryTrailDefinition.php
@@ -0,0 +1,43 @@
+<?php
+namespace DocumentExportDefinition\Section\Data;
+
+use DocumentExportDefinition\Section\AbstractDataDefinition;
+use JMS\Serializer\Annotation as Serializer;
+
+class BadgeCategoryTrailDefinition extends AbstractDataDefinition
+{
+	/**
+	 * @Serializer\SerializedName("value")
+	 * @Serializer\Type("array")
+	 * @var array
+	 */
+	protected $value;
+
+    /**
+     * @Serializer\VirtualProperty
+     * @Serializer\SerializedName("objectType")
+     * @Serializer\Type("string")
+     * @return string
+     */
+    public function getType()
+	{
+		return 'BadgeCategoryTrailDefinition';
+	}
+
+	/**
+	 * @param array $value
+	 */
+	public function setValue($value)
+	{
+		$this->value = $value;
+		return $this;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getValue() : array
+	{
+		return $this->value;
+	}
+}
